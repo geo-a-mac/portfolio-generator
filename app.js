494f7977f2002cec7eab0a2209019708749849c7
George Mcalpine <george.mcalpine@gmail.com>
2022-02-05 16:55:14 -0500

    validation

--- app.js
+++ app.js
@@ -14,12 +14,28 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your name? (required)',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your name!');
+                return false;
+            }
+        }
       },
       {
           type: 'input',
           name: 'github',
-          message: 'Enter your GitHub username'
+          message: 'Enter your GitHub username (required)',
+          validate: nameInput => {
+              if(nameInput) {
+                  return true;
+              } else {
+                  console.log('Please enter your GitHub username!');
+                  return false;
+              }
+          }
       },
       {
           type: 'input',
@@ -41,12 +57,28 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project? (required)',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the name of your project!');
+                return false;
+            }
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a description!');
+                return false;
+            }
+        }
       },
       {
         type: 'checkbox',
@@ -57,7 +89,15 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the GitHub link!');
+                return false;
+            }
+        }
       },
       {
         type: 'confirm',
